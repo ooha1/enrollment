a4ff8f331e7a7fa717d9f8eb08a9ad4239ffa5f9
vad <oohavadlamudi.31@gmail.com>
2025-05-09 11:38:00 -0500

    Code Refactoring.

--- src/main/java/com/studenthub/course/exception/ErrorResponse.java
+++ src/main/java/com/studenthub/course/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.studenthub.course.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,15 +9,9 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ErrorResponse {
     private LocalDateTime timeStamp;
     private String message;
     private List<String> details;
-
-    public ErrorResponse(LocalDateTime timeStamp, String message, List<String> details) {
-        this.timeStamp = timeStamp;
-        this.message = message;
-        this.details = details;
-    }
-
 }
